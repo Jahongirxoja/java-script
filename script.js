6e30773671bb9d4e1e68e39341a661a746bb8717
jahongir <jahongirisroilov2021@gmail.com>
2022-04-30 23:13:13 +0500

    qqq

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+ var str = prompt('Ismingizni kiriting')
+ var num = +prompt('Yoshingizni kiriting')
+
+ alert(('salom sizga misol beramiz ' + str + ' yoshingiz '+ num))
+
+
+
+
+
+var num2 = +prompt('6 + 3 =')
+var num3 = +prompt('6 - 3 =')
+var num4 = +prompt('6 + 5 =')
+var num5 = +prompt('6 * 5 =')
+var num6 = +prompt('25 / 5 =')
+var num7 = +prompt('10 % 3 =')
+
+
+
+
+
+console.info('ismingiz ' + str)
+console.info('yoshingiz ' + num)
+console.info('javob 6 + 3 = 6' + '(sizning javob ' +  num2 + ')') 
+console.info('javob 6 - 3 = 3' + ' sizning javob ' +  num3) 
+console.info('javob 6 + 5 = 11' + ' sizning javob ' +  num4) 
+console.info('javob 6 * 5 = 25' + ' sizning javob ' +  num4) 
+console.info('javob 25 / 5 = 5' + ' sizning javob ' +  num4) 
+console.info('javob 10 % 3 = 1' + ' sizning javob ' +  num4) 
